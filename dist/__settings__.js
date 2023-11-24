ccee2dde9b196bb8f02d6967f7b4885a890992a1
madjin <32600939+madjin@users.noreply.github.com>
2023-11-23 23:53:44 -0500

    add yard sale

---
+++ dist/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1887662.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 155007033, 155006901, 155007035, 155006833, 155007034, 155007032, 155006905, 155007052, 155007029, 155007028, 155006826, 155117952, 155119946 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/155007002/1/basis.wasm.js', 'wasmUrl' : 'files/assets/155007003/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/155007001/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/155007016/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/155007015/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/155007014/1/ammo.js', 'preload' : true},
+];
